278b5c6f06cfe7ffad490f02f5b4771dc5257409
Z. L. Noble <51288378+zlnoble@users.noreply.github.com>
2019-06-02 12:00:18 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+INSTRUCTIONS:
+
+1. Create your bot account just as you would a regular reddit account. Navigate to your preferences tab, then go to apps. Register your bot as a script. Put in dummy websites where it requires them (i.e. https://example.com). Your bot will be given 2 important character strings; a "secret" and an "id". Make a note of them; these values will be used later to fill in data for our program.
+
+2. Open up main.js. Fill in the data for myWrapper using the information from above, along with your reddit username and password.
+
+3. Change lines in 'reply_array' to the quotes you want your bot to say.
+
+4. Change the triggers from 'olenna' and 'queen of thorns' where the CommentStream is instantiated to whatever you want your bot to respond to. You can have as many or as few as you would like. Note that it is currently set to ignore case when searching for the trigger.
+
+5. Note that the subreddit is currently set to 'freefolk'. This, of course, can be changed to any subreddit.
+
+6. Make sure you have Node.js installed. If you don't, a quick google search will clue you in on how it's done. The process varies between operating systems. Node.js is very lightweight and easy to install.
+
+7. Once you have Node.js installed, open up a terminal/cmd prompt and navigate to the project's directory. Once you're in the project directory, type 'npm install' to install the project's dependencies.
+
+8. After that, if everything is done correctly, your bot should be ready to use. Just type  'node main.js' in the terminal/cmd window to run the application.
+
+
+NOTES:
+   - Early on, you may find error messages that pop up referencing a rate limit with a given timeout. This will happen while your bot has low karma; reddit is just throttling posting to prevent spam on new accounts.
+
+   - Your bot will only run so long as you have the application running. Should be obvious, but I thought I would point that out.
+
+   - I set up the bot to print out every time it receives a comment & replies to a comment. If you find this annoying, you can nix them in the code; they're not necessary for the program to run.
+
+   - If you have any further questions, feel free to message me @ reddit.com/user/olenna-bot
---
+++ main.js
@@ -0,0 +1,27 @@
+//created by: zlnoble
+//code available at: https://github.com/zlnoble/olenna-bot
+
+const Snoowrap = require('snoowrap');
+const {CommentStream} = require("snoostorm");
+
+const myWrapper = new Snoowrap({
+    userAgent: 'YOUR INFO GOES HERE',
+    clientId: 'YOUR INFO GOES HERE',
+    clientSecret: 'YOUR INFO GOES HERE',
+    username: 'YOUR INFO GOES HERE',
+    password: 'YOUR INFO GOES HERE',
+});
+
+myWrapper.config({requestDelay: 2000, continueAfterRatelimitError: true})
+
+const reply_array = ['Well, if it isn\'t the famous tart Queen Cersei.', 'A sword swallower through and through.', 'Tell Cersei. I want her to know it was me.', 'You look like an angry little boy. Don\'t presume to tell me what I need.', 'War is war, but killing a man at a wedding? Horrid.', 'I\'m leaving this wretched city as fast as I can before that shoeless zealot throws me into one of his cells.', 'Marrying a Targaryen was all the rage back then.', 'Do shut up, dear.', 'Anything from you? No? Good. Let the grown women speak.', 'The lords of Westeros are sheep. Are you a sheep? No. You\'re a dragon. Be a dragon.', 'Put the pen down dear, we both know you\'re not writing anything', 'He really was a cunt, wasn\'t he?', 'Get some rest, dear. You look appalling.',  'I wonder if you\’re the worst person I\’ve ever met.', 'What is your name again? Barbaro?', 'I was told you were drunk, impertinent, and thoroughly debauched.', 'The cheese will be served when I want it served, and I want it served now.', 'I\’ve known a great many clever men. I\’ve outlived them all. You know why? I ignored them.', 'But it\'s a natural thing, two boys having a go at each other beneath the sheets.', 'The world is overflowing with horrible things, but they\'re all a tray of cakes next to death.', 'My grandson is the pride of Highgarden. The most desirable bachelor in all seven kingdoms. Your daughter is...old.'];
+
+new CommentStream(myWrapper, {subreddit:'freefolk', pollTime:2000}).on('item', (item) => {
+	var timestamp = new Date().toLocaleString()
+	console.log('Comment received at: ' + timestamp)
+     	if (item.body.toLowerCase().includes('olenna') || item.body.toLowerCase().includes('queen of thorns')) { 
+        	var index = Math.floor(Math.random()*reply_array.length)
+        	item.reply(reply_array[index])
+		console.log('Reply generated.')
+     	}
+})
