9f8b7ce1695037d444c675924d980d4259fb686b
Z. L. Noble <51288378+zlnoble@users.noreply.github.com>
2019-06-05 18:12:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const reply_array = ['Well, if it isn\'t the famous tart Queen Cersei.', 'A swor
 		     'You look like an angry little boy. Don\'t presume to tell me what I need.', 'War is war, but killing a man at a wedding? Horrid.', 
 		     'I\'m leaving this wretched city as fast as I can before that shoeless zealot throws me into one of his cells.', 
 		     'Marrying a Targaryen was all the rage back then.', 'Do shut up, dear.', 'Anything from you? No? Good. Let the grown women speak.', 
-		     'The lords of Westeros are sheep. Are you a sheep? No. You\'re a dragon. Be a dragon.', 'Put the pen down dear, we both know you\'re not writing anything', 
+		     'The lords of Westeros are sheep. Are you a sheep? No. You\'re a dragon. Be a dragon.', 'Put the pen down dear, we both know you\'re not writing anything.', 
 		     'He really was a cunt, wasn\'t he?', 'Get some rest, dear. You look appalling.',  'I wonder if you\’re the worst person I\’ve ever met.', 'What is your name again? Barbaro?', 
 		     'I was told you were drunk, impertinent, and thoroughly debauched.', 'The cheese will be served when I want it served, and I want it served now.', 
 		     'I\’ve known a great many clever men. I\’ve outlived them all. You know why? I ignored them.', 'But it\'s a natural thing, two boys having a go at each other beneath the sheets.', 
